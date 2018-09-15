f2ce9e747c6ccd8625989babe981d1f922195e06
Luke Plausin <luke.plausin@gmail.com>
2018-09-15 21:29:56 +0200

    Added duckdns

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@
 username="luke"
 homedir="/home"
 
+DUCK_DOMAIN="lukeplausinpi"
+
 # Update, enable SSH
 apt-get update -y
 systemctl enable ssh
@@ -23,3 +25,13 @@ sudo apt-get install vim -y
 # Run RPI config
 # raspi-config
 
+# Setup duck DNS
+echo "DUCK_TOKEN=$DUCK_TOKEN"
+mkdir /var/log/duckdns/
+echo '#!/bin/bash' > /usr/local/bin/duckdns
+echo "echo url=\"https://www.duckdns.org/update?domains=$DUCK_DOMAIN&token=$DUCK_TOKEN&ip=\" | curl -s -k -o /var/log/duckdns/duck.log -K -" >> /usr/local/bin/duckdns
+chmod 700 /usr/local/bin/duckdns
+crontab -l > mycron
+echo "0 * * * * /usr/local/bin/duckdns >/dev/null 2>&1" >> mycron
+crontab mycron
+rm mycron
